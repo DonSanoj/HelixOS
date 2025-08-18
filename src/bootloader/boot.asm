10df29ed270f172a69b944fc9962d436ab9c35d8
DonSanoj <sanojaminda@gmail.com>
2025-08-18 10:22:21 +0530

    add bochs config and shells for run

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -1,38 +1,51 @@
 org 0x7C00
 bits 16
 
+
 %define ENDL 0x0D, 0x0A
 
-; Fat12 header
+
+;
+; FAT12 header
+; 
 jmp short start
-nop 
+nop
 
-bdb_oem:                        db 'MSWIN4.1'           ; 8 bytes
-bdb_bytes_per_sector:           dw 512
-bdb_sectors_per_cluster:        db 1
-bdb_reserved_sectors:           dw 1
-bdb_fat_count:                  db 2
-bdb_dir_entries_count:          dw 0E0h
-bdb_total_sectors:              dw 2880
-bdb_media_descriptor_type:      db 0F0h
-bdb_sectors_per_fat:            dw 9
-bdb_sectors_per_track:          dw 18
-bdb_heads:                      dw 2
-bdb_hidden_sectors:             dd 0
-bdb_large_sector_count:         dd 0
+bdb_oem:                    db 'MSWIN4.1'           ; 8 bytes
+bdb_bytes_per_sector:       dw 512
+bdb_sectors_per_cluster:    db 1
+bdb_reserved_sectors:       dw 1
+bdb_fat_count:              db 2
+bdb_dir_entries_count:      dw 0E0h
+bdb_total_sectors:          dw 2880                 ; 2880 * 512 = 1.44MB
+bdb_media_descriptor_type:  db 0F0h                 ; F0 = 3.5" floppy disk
+bdb_sectors_per_fat:        dw 9                    ; 9 sectors/fat
+bdb_sectors_per_track:      dw 18
+bdb_heads:                  dw 2
+bdb_hidden_sectors:         dd 0
+bdb_large_sector_count:     dd 0
 
-; extended boot sector
-ebr_drive_number:               db 0
-                                db 0                    ; reserved
-ebr_signature:                  db 29h
-ebr_volume_id:                  db 12h, 34h, 56h, 78h
-ebr_volume_label:               db 'HELIXOS    '        ; 11 bytes
-ebr_system_id:                  db 'FAT12   '           ; 8 bytes
+; extended boot record
+ebr_drive_number:           db 0                    ; 0x00 floppy, 0x80 hdd, useless
+                            db 0                    ; reserved
+ebr_signature:              db 29h
+ebr_volume_id:              db 12h, 34h, 56h, 78h   ; serial number, value doesn't matter
+ebr_volume_label:           db 'NANOBYTE OS'        ; 11 bytes, padded with spaces
+ebr_system_id:              db 'FAT12   '           ; 8 bytes
 
+;
+; Code goes here
+;
 
 start:
     jmp main
 
+
+;
+; Prints a string to the screen
+; Params:
+;   - ds:si points to string
+;
 puts:
     ; save registers we will modify
     push si
@@ -40,12 +53,12 @@ puts:
     push bx
 
 .loop:
-    lodsb           ; load next character in al
-    or al, al       ; verify if next character is null?
+    lodsb               ; loads next character in al
+    or al, al           ; verify if next character is null?
     jz .done
 
-    mov ah, 0x0E
-    mov bh, 0
+    mov ah, 0x0E        ; call bios interrupt
+    mov bh, 0           ; set page number to 0
     int 0x10
 
     jmp .loop
@@ -53,33 +66,40 @@ puts:
 .done:
     pop bx
     pop ax
-    pop si
+    pop si    
     ret
+    
 
 main:
-
     ; setup data segments
-    mov ax, 0       ; can't write to ds/es directly
+    mov ax, 0                   ; can't set ds/es directly
     mov ds, ax
     mov es, ax
-
+    
     ; setup stack
     mov ss, ax
-    mov sp, 0x7c00
+    mov sp, 0x7C00              ; stack grows downwards from where we are loaded in memory
 
     ; read something from floppy disk
     ; BIOS should set DL to drive number
     mov [ebr_drive_number], dl
 
-    mov ax, 1
-    mov cl, 1
-    mov bx, 
+    mov ax, 1                   ; LBA=1, second sector from disk
+    mov cl, 1                   ; 1 sector to read
+    mov bx, 0x7E00              ; data should be after the bootloader
     call disk_read
 
+    ; print hello world message
     mov si, msg_hello
     call puts
 
+    cli                         ; disable interrupts, this way CPU can't get out of "halt" state
     hlt
+
+
+;
+; Error handlers
+;
 
 floppy_error:
     mov si, msg_read_failed
@@ -88,12 +108,13 @@ floppy_error:
 
 wait_key_and_reboot:
     mov ah, 0
-    int 16h 
-    jmp 0FFFFh:0
+    int 16h                     ; wait for keypress
+    jmp 0FFFFh:0                ; jump to beginning of BIOS, should reboot
 
 .halt:
-    cli
+    cli                         ; disable interrupts, this way CPU can't get out of "halt" state
     hlt
+
 
 ;
 ; Disk routines
@@ -107,73 +128,91 @@ wait_key_and_reboot:
 ;   - cx [bits 0-5]: sector number
 ;   - cx [bits 6-15]: cylinder
 ;   - dh: head
+;
 
 lba_to_chs:
 
     push ax
     push dx
 
-    xor dx, dx
-    div word [bdb_sectors_per_track]
+    xor dx, dx                          ; dx = 0
+    div word [bdb_sectors_per_track]    ; ax = LBA / SectorsPerTrack
+                                        ; dx = LBA % SectorsPerTrack
 
-    inc dx
-    mov cx, dx
+    inc dx                              ; dx = (LBA % SectorsPerTrack + 1) = sector
+    mov cx, dx                          ; cx = sector
 
-    xor dx, dx
-    div word [bdb_heads]
-
-    mov dh, dl
-    mov ch, al
+    xor dx, dx                          ; dx = 0
+    div word [bdb_heads]                ; ax = (LBA / SectorsPerTrack) / Heads = cylinder
+                                        ; dx = (LBA / SectorsPerTrack) % Heads = head
+    mov dh, dl                          ; dh = head
+    mov ch, al                          ; ch = cylinder (lower 8 bits)
     shl ah, 6
-    or cl, ah
+    or cl, ah                           ; put upper 2 bits of cylinder in CL
 
     pop ax
-    mov dl, al
+    mov dl, al                          ; restore DL
     pop ax
     ret
 
+
+;
+; Reads sectors from a disk
+; Parameters:
+;   - ax: LBA address
+;   - cl: number of sectors to read (up to 128)
+;   - dl: drive number
+;   - es:bx: memory address where to store read data
+;
 disk_read:
 
-    push ax
+    push ax                             ; save registers we will modify
     push bx
     push cx
     push dx
     push di
 
-    push cx
-    call lba_to_chs
-    pop ax
-
+    push cx                             ; temporarily save CL (number of sectors to read)
+    call lba_to_chs                     ; compute CHS
+    pop ax                              ; AL = number of sectors to read
+    
     mov ah, 02h
-    mov di, 3
+    mov di, 3                           ; retry count
 
 .retry:
-    pusha
-    stc 
-    int 13h
-    jnc .done
+    pusha                               ; save all registers, we don't know what bios modifies
+    stc                                 ; set carry flag, some BIOS'es don't set it
+    int 13h                             ; carry flag cleared = success
+    jnc .done                           ; jump if carry not set
 
     ; read failed
     popa
     call disk_reset
 
     dec di
-    test di, di 
+    test di, di
     jnz .retry
 
 .fail:
+    ; all attempts are exhausted
     jmp floppy_error
 
 .done:
     popa
 
-    push di
-    push dx
-    push cx
-    push bx
-    push ax
+    pop di
+    pop dx
+    pop cx
+    pop bx
+    pop ax                             ; restore registers modified
     ret
 
+
+;
+; Resets disk controller
+; Parameters:
+;   dl: drive number
+;
 disk_reset:
     pusha
     mov ah, 0
@@ -183,9 +222,9 @@ disk_reset:
     popa
     ret
 
+
 msg_hello:              db 'Hello world!', ENDL, 0
 msg_read_failed:        db 'Read from disk failed!', ENDL, 0
-
 
 times 510-($-$$) db 0
 dw 0AA55h
